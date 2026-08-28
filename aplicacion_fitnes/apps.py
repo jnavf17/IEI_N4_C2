9e245efe6511e9f7866491d73f7b7d7e2a043782
jnavf17 <jignacionavarretef14@gmail.com>
2026-08-28 14:30:08 -0400

    jnavf17

---
+++ aplicacion_fitnes/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AplicacionFitnesConfig(AppConfig):
+    name = 'aplicacion_fitnes'
--- bkend_aplicacion_fitnes/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BkendAplicacionFitnesConfig(AppConfig):
-    name = 'bkend_aplicacion_fitnes'
