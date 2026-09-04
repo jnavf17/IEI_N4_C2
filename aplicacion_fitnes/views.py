7a2820c0985be1e9be1807c505291dc5e64b29f7
jnavf17 <jignacionavarretef14@gmail.com>
2026-09-03 23:19:34 -0400

    actualizacion

--- aplicacion_fitnes/views.py
+++ aplicacion_fitnes/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+def inicio(request):
+    return render(request, 'aplicacion_fitnes/inicio.html')
 
 # Create your views here.
+
+#def inicio(request):
+    #return HttpResponse('Bienvenidos a la aplicacion de fitnes')
